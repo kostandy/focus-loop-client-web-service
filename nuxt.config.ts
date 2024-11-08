714c3abd22192f4d6eda025bc5f13b1362cb4aa1
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-11-09 00:17:40 +0200

    chore(config)!:disable telemetry and update compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   typescript: {
     typeCheck: true
+  },
+
+  telemetry: {
+    enabled: false
   },
 
   modules: ['@nuxt/eslint']

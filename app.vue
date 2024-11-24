9414dd0c36850ceaea677a59cfbe4b1f5c856f39
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-11-24 16:31:55 +0200

    feat(tailwind): add new InterVariable font

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  link: [{
+    rel: "preconnect",
+    href: "https://rsms.me"
+  }, {
+    rel: "stylesheet",
+    href: "https://rsms.me/inter/inter.css"
+  }]
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>

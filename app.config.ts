f2d1d555d228809a947d87fb12957ede5b454b74
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-11-27 00:59:36 +0200

    chore: override some default styles

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+    ui: {
+        primary: 'blue',
+        gray: 'slate',
+        button: {
+            default: {
+                size: "lg"
+            }
+        },
+    },
+})

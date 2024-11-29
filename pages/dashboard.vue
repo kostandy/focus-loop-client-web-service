9a0ac69d68088b2f9a7d4a8c87108cb75440e001
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-11-29 16:53:01 +0200

    chore: update add task appearance

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -39,18 +39,18 @@ const submitForm = async (newTask: Task) => {
 }
 
 const launchConfetti = () => {
-  confetti({
-    particleCount: 100,
-    spread: 70,
-    origin: { x: 0.5, y: 0.5 },
-  });
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: 0.5, y: 0.5 },
+    });
 };
 
 const setTaskStatus = (id: Task['id'], newStatus: TaskStatuses) => {
     const task = taskStore.getTaskById(id);
     if (task) {
         taskStore.update(id, { ...task, status: newStatus });
-        
+
         if (newStatus === TaskStatuses.completed) {
             taskCompletitionSound?.value?.play();
             launchConfetti()
@@ -62,13 +62,16 @@ const setTaskStatus = (id: Task['id'], newStatus: TaskStatuses) => {
 <template>
     <UContainer as="main">
         <div class="flex items-end justify-between my-4">
-            <UButton :to="tonviewerWalletLink" label="Donate 1 TON" class="flex w-auto" target="_blank" leading-icon="i-heroicons-heart" size="md" variant="link" external block />
+            <UButton :to="tonviewerWalletLink" label="Donate 1 TON" class="flex w-auto" target="_blank"
+                leading-icon="i-heroicons-heart" size="md" variant="link" external block />
 
             <UButton icon="i-heroicons-cog-6-tooth" variant="ghost" disabled />
         </div>
 
         <div class="sticky top-4 my-6 mx-2 z-20">
-            <UButton label="Add a Task" class="md:w-auto font-bold shadow-lg rounded-full" trailing-icon="i-heroicons-plus-solid" block :disabled="taskStore.isLoading" @click="openNewTaskModal" />
+            <UButton label="Add a Task" class="md:w-auto font-bold shadow-lg bg-slate-900 border rounded-full"
+                trailing-icon="i-heroicons-plus-solid" variant="ghost" color="white" block :disabled="taskStore.isLoading"
+                @click="openNewTaskModal" />
         </div>
 
         <b v-if="taskStore.isLoading">Loading your tasks...</b>
@@ -76,14 +79,10 @@ const setTaskStatus = (id: Task['id'], newStatus: TaskStatuses) => {
         <div v-else-if="!taskStore.isLoading && taskStore.tasks && !taskStore.fetchError" class="relative mx-1 z-10">
             <template v-if="!taskStore.tasks.length">No tasks are available</template>
             <template v-else>
-                <TaskCard
-                    v-for="task in taskStore.tasks"
-                    v-bind="task"
-                    class="mb-8"
-                    @change-status="payload => setTaskStatus(task.id, payload)"
-                    @remove="taskStore.remove"
-                />
-                <div v-if="taskStore.tasks.length > 4" class="fixed bottom-0 left-0 right-0 bg-gradient-to-t dark:from-slate-900 h-10"></div>
+                <TaskCard v-for="task in taskStore.tasks" v-bind="task" class="mb-8"
+                    @change-status="payload => setTaskStatus(task.id, payload)" @remove="taskStore.remove" />
+                <div v-if="taskStore.tasks.length > 4"
+                    class="fixed bottom-0 left-0 right-0 bg-gradient-to-t dark:from-slate-900 h-10"></div>
             </template>
         </div>
 

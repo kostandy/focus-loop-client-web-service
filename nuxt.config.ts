3a44cf0ceb122743759a769eb01a576f8bd290f5
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-12-04 22:29:06 +0200

    feat: add husky, lint-staged, enable eslint checker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,57 +1,56 @@
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   colorMode: {
-    preference: 'dark'
+    preference: "dark",
   },
 
-  compatibilityDate: '2024-12-01',
+  compatibilityDate: "2024-12-01",
 
-  devtools: { enabled: process.env.NODE_ENV === 'development' },
+  devtools: { enabled: process.env.NODE_ENV === "development" },
 
-  modules: [
-    '@nuxt/eslint',
-    '@pinia/nuxt',
-    '@nuxt/ui',
-    '@sentry/nuxt/module'
-  ],
+  eslint: {
+    checker: true,
+  },
+
+  modules: ["@nuxt/eslint", "@pinia/nuxt", "@nuxt/ui", "@sentry/nuxt/module"],
 
   router: {
     options: {
-      hashMode: true
-    }
+      hashMode: true,
+    },
   },
 
   runtimeConfig: {
     public: {
       sentry: {
         dsn: process.env.SENTRY_DSN,
-        release: execSync('git rev-parse HEAD').toString().trim()
-      }
-    }
+        release: execSync("git rev-parse HEAD").toString().trim(),
+      },
+    },
   },
 
   ssr: false,
 
   telemetry: {
-    enabled: false
+    enabled: false,
   },
 
   typescript: {
-    typeCheck: true
+    typeCheck: true,
   },
 
   sentry: {
     sourceMapsUploadOptions: {
       org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT
-    }
+      project: process.env.SENTRY_PROJECT,
+    },
   },
 
-  spaLoadingTemplate: 'spa-loading-template.html',
+  spaLoadingTemplate: "spa-loading-template.html",
 
   sourcemap: {
-    client: 'hidden'
-  }
-})
+    client: "hidden",
+  },
+});

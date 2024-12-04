5af6bc789abaed2f326841d2a0289a782b39f743
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-12-05 01:09:44 +0200

    fix(utils): resolve TS error + format a file

--- utils/index.ts
+++ utils/index.ts
@@ -1,22 +1,30 @@
-export const generateId = () => Date.now().toString(36) + Math.random().toString(36).substring(2);
-export const loadAudio = (audioFile: string): HTMLAudioElement => new Audio(audioFile);
-export const throttle = (func: Function, limit: number) => {
-    let lastFunc: ReturnType<typeof setTimeout>;
-    let lastRan: number;
+export const generateId = (): string =>
+  Date.now().toString(36) + Math.random().toString(36).substring(2);
+export const loadAudio = (audioFile: string): HTMLAudioElement =>
+  new Audio(audioFile);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const throttle = <T extends (...args: any[]) => any>(
+  func: T,
+  limit: number,
+): ((...args: Parameters<T>) => void) => {
+  let lastFunc: ReturnType<typeof setTimeout>;
+  let lastRan: number;
 
-    return (...args: any[]) => {
-        const context = this;
-        if (!lastRan) {
-            func.apply(context, args);
+  return (...args: Parameters<T>) => {
+    if (!lastRan) {
+      func.apply(this, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(
+        () => {
+          if (Date.now() - lastRan >= limit) {
+            func.apply(this, args);
             lastRan = Date.now();
-        } else {
-            clearTimeout(lastFunc);
-            lastFunc = setTimeout(() => {
-                if ((Date.now() - lastRan) >= limit) {
-                    func.apply(context, args);
-                    lastRan = Date.now();
-                }
-            }, limit - (Date.now() - lastRan));
-        }
-    };
+          }
+        },
+        limit - (Date.now() - lastRan),
+      );
+    }
+  };
 };

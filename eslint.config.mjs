5728a7a28e740b335881d53932baa97c2f41fada
Dmytro Andriushchenko <dmytro.andriushchenko@slim.ai>
2024-12-05 04:06:49 +0200

    chore(deps): add eslint-plugin-vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginVue from "eslint-plugin-vue";
 
-export default withNuxt(
-  // Your custom configs here
-)
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt([
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["pages/**/*.vue", "layouts/**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+]);

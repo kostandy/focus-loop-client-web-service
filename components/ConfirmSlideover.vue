5a950eac58ba0b845f0f0fee2902f4e29e96416d
Dmytro Andriushchenko <spaser15@outlook.com>
2025-02-03 18:16:16 +0200

    fix(ConfirmSlideover): fix container's width

--- components/ConfirmSlideover.vue
+++ components/ConfirmSlideover.vue
@@ -14,27 +14,36 @@ const onClose = () => emit('close');
 <template>
 <USlideover
 	side="bottom"
-	:ui="{ base: 'rounded-t-3xl duration-500 overflow-hidden', height: '' }"
+	:ui="{
+		wrapper: 'h-full',
+		base: 'rounded-t-3xl duration-500 overflow-hidden',
+		height: 'h-auto max-h-dvh',
+	}"
 	@close="onClose"
 >
-	<UCard class="px-2">
-		<div class="space-y-4">
-			<p>
-				Are you sure you want to remove <b>{{ title }}</b>?
-			</p>
+	<UContainer class="w-full">
+		<UCard
+			:ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }"
+			class="px-2"
+		>
+			<div class="space-y-4">
+				<p>
+					Are you sure you want to remove <b>{{ title }}</b>?
+				</p>
 
-			<p class="my-4">
-				This action cannot be undone
-			</p>
+				<p class="my-4">
+					This action cannot be undone
+				</p>
 
-			<UButton
-				color="rose"
-				block
-				@click="onSuccess"
-			>
-				Yes, remove it
-			</UButton>
-		</div>
-	</UCard>
+				<UButton
+					color="rose"
+					block
+					@click="onSuccess"
+				>
+					Yes, remove it
+				</UButton>
+			</div>
+		</UCard>
+	</UContainer>
 </USlideover>
 </template>
